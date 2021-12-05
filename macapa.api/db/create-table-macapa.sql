65b74505f282be1b49d63666c9575a424df5c5d7
Higor Castilho <castilhohf@gmail.com>
2021-12-05 04:46:39 -0300

    implement contact post endpoint to macapa.api and initial database, contact table and seed

--- macapa.api/db/create-table-macapa.sql
+++ macapa.api/db/create-table-macapa.sql
@@ -4,9 +4,9 @@ use macapadb;
 CREATE TABLE `contact`(
 	`id` int unsigned NOT NULL AUTO_INCREMENT,
 	`name` varchar(200) NOT NULL,
-	`phone` varchar (20) NOT NULL,
+	`cellphone` varchar (20) NOT NULL,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-insert into `contact`(`name`, `phone`) values
+insert into `contact`(`name`, `cellphone`) values
 ('João', '223333453')
